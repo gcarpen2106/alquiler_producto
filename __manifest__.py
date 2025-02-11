0ecfeacbb12c34ef508f98fa8da17c10df2fd814
gcarpen2106 <gcarpen2106@gmail.com>
2025-02-11 09:51:32 +0100

    Editando el archivo __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# __manifest__.py
+
+{
+    'name': 'Alquiler de Productos',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Gestión de alquiler de productos',
+    'description': 'Gestión de préstamos de productos en alquiler para clientes',
+    'author': 'Gonzalo Carretero',
+    'depends': ['base', 'sale', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/alquiler_producto_views.xml'
+    ],
+    'icon': '/alquiler_producto/static/description/icon.png',
+    'installable': True,
+    'application': True,
+}
